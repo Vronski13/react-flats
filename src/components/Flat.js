2471b6cbed1ac14f475c88ab9278e084c08c2957
vmueller <vmueller@vmuellers-MBP.fritz.box>
2020-07-17 15:18:02 +0200

    added inline styling for flat component

--- src/components/Flat.js
+++ src/components/Flat.js
@@ -4,18 +4,24 @@ import React, { Component } from 'react';
 class Flat extends Component {
     render() {
         const flat = this.props.flat;
+        const url = flat.imageUrl
+        const styles = {
+          backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(" + url + ")"
+        };
+        console.log(styles)
         return (
           <div
-            className="card"
+            className="card" style={styles}
           >
             <div className="card-category">{flat.price} {flat.priceCurrency}</div>
             <div className="card-description">
               <h2>{flat.name}</h2>
             </div>
-            <a className="card-link" href={flat.imageUrl}></a>
+            <a className="card-link" href="#"></a>
           </div>
         );
     }
 }
 
 export default Flat;
+  
